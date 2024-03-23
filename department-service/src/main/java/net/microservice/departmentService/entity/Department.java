c4b21805cede066af1625d47b1d62443bd445996
Ajay <ajay.singh.114085@gmail.com>
2024-03-23 19:26:07 +0530

    JPA entity and repsitory

---
+++ department-service/src/main/java/net/microservice/departmentService/entity/Department.java
@@ -0,0 +1,22 @@
+package net.microservice.departmentService.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String deparmentCode;
+}
---
+++ department-service/src/main/java/net/microservice/departmentService/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package net.microservice.departmentService.repository;
+
+import net.microservice.departmentService.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+
+}
