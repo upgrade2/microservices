8cc39c71e302ac0b1158105731a0e20678811c23
Ajay <ajay.singh.114085@gmail.com>
2024-03-23 16:19:55 +0530

    department/employee services.

---
+++ department-service/src/main/java/net/microservice/departmentService/DepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package net.microservice.departmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentServiceApplication.class, args);
+	}
+
+}
---
+++ employee-service/src/test/java/net/microservice/employeeService/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.microservice.employeeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
