a340bece63266746b80a2d1d8ddc64a6e8bf716c
Ajay <ajay.singh.114085@gmail.com>
2024-03-24 22:04:47 +0530

    employee service calling departmentservice with WebClient

--- employee-service/src/main/java/net/microservice/employeeService/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/microservice/employeeService/service/impl/EmployeeServiceImpl.java
@@ -10,13 +10,16 @@ import net.microservice.employeeService.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+   // private RestTemplate restTemplate;
+
+    private WebClient webClient;
     /**
      * @param employeeDto
      * @return
@@ -58,9 +61,16 @@ public class EmployeeServiceImpl implements EmployeeService {
     public APIResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
 
-       ResponseEntity<DepartmentDto> responseEntity =restTemplate.getForEntity("http://localhost:8080/api/department/"+employee.getDepartmentCode(), DepartmentDto.class);
+//       ResponseEntity<DepartmentDto> responseEntity =restTemplate.getForEntity("http://localhost:8080/api/department/"+employee.getDepartmentCode(), DepartmentDto.class);
+// DepartmentDto departmentDto =responseEntity.getBody();
 
-       DepartmentDto departmentDto =responseEntity.getBody();
+        //call from WebClient
+       DepartmentDto departmentDto = webClient.get().uri("http://localhost:8080/api/department/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+
+
 
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
